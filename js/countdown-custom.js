51c3a55b7172dad8ef4ec232660124cf96ed0600
uditchrist <udit.gupta@mca.christuniversity.in>
2020-10-28 17:07:33 +0530

    Commit by Udit

---
+++ js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date(2020, 10, 20, 8)}); // year, month, date, hour
+        });
+});		
+
